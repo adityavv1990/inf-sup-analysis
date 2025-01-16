2c8556a14755f9b511004f510e51a8c4b6e61c85
adityavv1990 <adityavv.iitkgp@gmail.com>
2025-01-16 18:15:15 +0100

    adding more functions to the analysis

--- inf_sup.py
+++ inf_sup.py
@@ -7,11 +7,13 @@ Created on Fri Sep 20 14:34:40 2024
 
 # Este archivo contiene la función mixed_infsup
 import numpy as np
-from scipy.sparse.linalg import eigsh, spsolve, LinearOperator, ArpackNoConvergence, eigs, lobpcg, inv, splu, norm
+from scipy.sparse.linalg import eigsh, spsolve, LinearOperator, ArpackNoConvergence, eigs, lobpcg, inv, splu, norm, svds
 from scipy.linalg import sqrtm, matrix_balance
 from scipy.sparse import csc_matrix
 import time, math, sys
 from scipy.linalg import eigh, inv
+from cvxpy.atoms.affine.wraps import psd_wrap
+
 
 
 def is_positive_definite_sparse(matrix):
@@ -111,20 +113,7 @@ def mixed_infsup(matB, matH, matL):
     matH = matH.astype(np.float64)
     matL = matL.astype(np.float64)
 
-    Bdense = matB.toarray()
-    Hdense = matH.toarray()
-    Ldense = matL.toarray()
-
-    Hinv = np.linalg.inv(Hdense)
-    BHinvBT = Bdense @ Hinv @ Bdense.T
-
-    # We will evaluate the rank of the matrix B*H^(-1)*B^T
-    rank = np.linalg.matrix_rank(BHinvBT)
-    m,m = BHinvBT.shape
-
-    print("Dimension of the square matrix B*H^(-1)*B^T = ", m, flush=True)
-    print("Rank of B*H^(-1)*B^T = ", rank, flush=True)
-
+    m,n = matB.shape
 
     def operador_BHinvBt(B, H):
       
@@ -150,13 +139,21 @@ def mixed_infsup(matB, matH, matL):
     
     try:
         # Calculamos el menor autovalor
-        mineig, _ = eigsh(A = operator, k = m-1, M = matL, which = 'SA', tol = 1e-5)
+        eigValues, _ = eigsh(A = operator, k = m-1, M = matL, which = 'SA', tol = 1e-5)
+        eigValueMax, _ = eigsh(A = operator, k = 1, M = matL, which = 'LA', tol = 1e-5)
 
-        print("Eigenvalues = ", mineig, flush=True)
-        print("shape of eigenvalues = ", mineig.shape, flush=True)
+        eigValues = np.append(eigValues, eigValueMax)
+
+        rank = (abs(eigValues) > 1e-5).sum()
+
+        print("Eigenvalues = ", eigValues, flush=True)
+        print("MAX Eigenvalue = ", eigValueMax, flush=True)
+        print("shape of eigenvalues = ", eigValues.shape, flush=True)
         print("m = ", m, flush=True)
-        mineigenValue = mineig[m-rank]
-        maxeigenValue = mineig[-1]
+        print("rank of the matrix B H^-1 B.T= ", rank, flush=True)
+
+        mineigenValue = eigValues[m-rank]
+        maxeigenValue = eigValues[-1]
         eigenValues = [mineigenValue, maxeigenValue]
         return eigenValues 
         
@@ -198,24 +195,7 @@ def mixed_infsup_C(matB, matH, matC):
     matH = matH.astype(np.float64)
     matC = matC.astype(np.float64)
 
-    Bdense = matB.toarray()
-    Hdense = matH.toarray()
-    Cdense = matC.toarray()
-
-    Cinv = np.linalg.inv(Cdense)
-    BtCinvB = Bdense.T @ Cinv @ Bdense
-
-    # We will evaluate the rank of the matrix B*H^(-1)*B^T
-    rank = np.linalg.matrix_rank(BtCinvB)
-    n,n = BtCinvB.shape
-
-    rankB = np.linalg.matrix_rank(Bdense)
-
-    print("Dimension of the square matrix B^T*C^(-1)*B = ", n, flush=True)
-    print("Rank of  B^T*C^(-1)*B = ", rank, flush=True)
-    print("Dimension of the matrix B = ", Bdense.shape, flush=True)
-    print("Rank of  B = ", rankB, flush=True)
-
+    m,n = matB.shape
 
     def operador_BtCinvB(B, C):
       
@@ -241,13 +221,20 @@ def mixed_infsup_C(matB, matH, matC):
     
     try:
         # Calculamos el menor autovalor
-        mineig, _ = eigsh(A = operator, k = n-1, M = matH, which = 'SA', tol = 1e-5)
+        eigValues, _ = eigsh(A = operator, k = n-1, M = matH, which = 'SA', tol = 1e-5)
+        eigValuesMax, _ = eigsh(A = operator, k = 1, M = matH, which = 'LA', tol = 1e-5)
 
-        print("Eigenvalues = ", mineig, flush=True)
-        print("shape of eigenvalues = ", mineig.shape, flush=True)
+        eigValues = np.append(eigValues, eigValuesMax)
+
+        rank = (abs(eigValues) > 1e-1).sum()
+
+        print("Eigenvalues = ", eigValues, flush=True)
+        print("shape of eigenvalues = ", eigValues.shape, flush=True)
         print("n = ", n, flush=True)
-        mineigenValue = mineig[n-rank]
-        maxeigenValue = mineig[-1]
+        print("rank of the matrix B.T C^-1 B= ", rank, flush=True)
+
+        mineigenValue = eigValues[n-rank]
+        maxeigenValue = eigValues[-1]
         eigenValues = [mineigenValue, maxeigenValue]
         return eigenValues 
         
@@ -262,7 +249,7 @@ def mixed_infsup_C(matB, matH, matC):
 
 
 
-def primal_infsup(matM, matH, eps):
+def primal_infsup(matM, matH, eps = 0.0):
     """
     Calcula el valor de la constante inf-sup de la matriz M de una
     discretización dada. Lo hace a partir de la resolución del problema
@@ -378,10 +365,188 @@ def primal_infsup(matM, matH, eps):
 
         #mineig2 = eigh(regularized_M, regularized_H, eigvals_only=True, subset_by_index = [0,0], driver='gvx')
         allValues, _ = eigsh(A =  regularized_M_sparse, k = N-1, M = regularized_H_sparse, which = 'SA', tol = 1e-3, maxiter=N*200)
+        eigMaxValue, _ = eigsh(A =  regularized_M_sparse, k = 1, M = regularized_H_sparse, which = 'LA', tol = 1e-3, maxiter=N*200)
+        allValues = np.append(allValues, eigMaxValue)
 
         allValues = allValues * scale_M/scale_H
 
-        minEigenValue = allValues[0]
+        rankM = (abs(allValues) > 1e-3).sum()
+
+        print("Eigenvalues = ", allValues, flush=True)
+        print("shape of eigenvalues = ", allValues.shape, flush=True)
+        print("n = ", N, flush=True)
+        print("rank of the matrix A = ", rankM, flush=True)
+
+
+        minEigenValue = allValues[N-rankM]
+        maxEigenValue = allValues[-1]
+        eigenValues = [minEigenValue, maxEigenValue]
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Elapsed time in balanced matrix block: {elapsed_time:.6f} seconds", flush=True)
+
+    except ArpackNoConvergence:
+        print("Error de convergencia")
+        eigenValues = [0.0, 0.0]
+
+    return eigenValues
+
+
+
+
+def primal_infsup_onKerB(matM, matH, matB, eps = 0.0):
+    """
+    Calcula el valor de la constante inf-sup de la matriz M de una
+    discretización dada. Lo hace a partir de la resolución del problema
+    de autovalor generalizado B * H^(-1) * B^T * x = lambda * L * x. 
+    
+    Nos ayuda en el estudio de la estabilidad de una formulación.
+
+    Parameters
+    ----------
+    matB : scipy.sparse matrix
+        Matriz asociada a la forma bilineal B. De dimensiones (m, n)
+    matH : scipy.sparse matrix
+        Matriz de norma primal. Es simétrica y definida positiva, de
+        dimensiones (n, n)
+    matL : scipy.sparse matrix
+        Matriz de norma dual. Es simétrica y definida positiva, de
+        dimensiones (m, m)
+
+    Returns
+    -------
+    float
+        La raíz cuadrada del menor autovalor del problema de autovalor
+        generalizado B * H^(-1) * B^T * x = lambda * L * x.
+        
+        Si el cálculo no converge, devuelve un mensaje de error de convergencia 
+        ('Error de convergencia').
+
+    """
+    
+    matM = matM.astype(np.float64)
+    matH = matH.astype(np.float64)
+    matB = matB.astype(np.float64)
+
+    n, n = matM.shape
+
+    ## We will verify if H and M are positive definite or not
+    checkpositiveDefiniteness(matM, matH)
+    matM = symmetrizeMatrix(matM)
+    matH = symmetrizeMatrix(matH)
+    
+    ## We will next evaluate the kernel of the B matrix
+    m , n = matB.shape
+    print("The shape of B = ", matB.shape, flush=True)
+
+
+    print("Calculating the null space of B", flush=True)
+    u , s, vt = svds(matB, k = m-1, tol = 1e-5, which = 'SM')
+    
+    rankB = (s > 1e-5).sum()
+    print("The rank of B = ", rankB, flush=True)
+    nullspaceindices = np.where(s < 1e-5)
+    nullSpace = vt[nullspaceindices].T
+
+    print("The null space of B = ", nullSpace, flush=True)
+    print("The shape of the null space of B = ", nullSpace.shape, flush=True)
+
+    # Projection matrix
+    temp = nullSpace.T @ nullSpace
+    tempInv = np.linalg.inv(temp)
+    print("tempInv = ", tempInv, flush=True)
+    P = nullSpace @ tempInv @ nullSpace.T
+    print("P = ", P, flush=True)
+
+    
+    # convert H from sparse to dense and take square root and convert it back
+    #Hdense = matH.toarray()
+    
+    
+    #start_time = time.time()
+    #sqrtHdense = sqrtm(Hdense)
+    #end_time = time.time()
+    #elapsed_time = end_time-start_time
+    #print(f"Elapsed time in calculating sqrt(H): {elapsed_time:.6f} seconds", flush=True)
+
+
+    #sqrtH = csc_matrix(sqrtHdense)
+
+
+    #operator = operador_HnegHalf_M_HnegHalf(sqrtH, matM)
+    
+    allValues = np.ndarray([], dtype=float)
+    eigenValues = []
+
+    # try:
+    #     # Calculamos el menor autovalor
+    #     start_time = time.time()
+    #     print("Evaluating the eigenvalue using linear operator-------------->",flush=True)        
+    #     #MtM = matM.T @ matM
+    #     mineig1, _ = eigsh(A = operator, k = 1, which = 'SM', tol = 1e-5)
+    #     print("Minimum-eigenvalue from operator = ", mineig1, flush=True)
+    #     end_time = time.time()
+    #     elapsed_time = end_time - start_time
+    #     print(f"Elapsed time in linear operator block: {elapsed_time:.6f} seconds", flush=True)
+        
+    # except ArpackNoConvergence:    
+    #     print("Error de convergencia")
+    #     mineig1 = np.array([0.0])
+    
+    try: 
+        start_time = time.time()
+        print("Evaluating the eigenvalue direct matrix evaluation-------------->", flush=True)        
+        #Hminushalf = inv(sqrtHdense)
+        #HminushalfSparse = csc_matrix(Hminushalf)        
+        #Mtilde = HminushalfSparse @ matM @ HminushalfSparse
+        #MtildeDense = Mtilde.toarray()
+        #MtildeBalanced, permscale = matrix_balance(MtildeDense)
+        #print(permscale, flush=True)
+        #MtildeBalanceSparse = csc_matrix(MtildeBalanced)
+        #mineig2, _ = eigsh(MtildeBalanceSparse, k = 1, which = 'SM', tol = 1e-3, maxiter=N)
+        Mdense = matM.toarray()
+        Hdense = matH.toarray()
+
+        PMP = P @ Mdense @ P
+        PHP = P @ Hdense @ P
+
+        print("PMP = ", PMP, flush=True)
+        print("PHP = ", PHP, flush=True)
+
+        #mineig2, _ = eigsh(A =  matM, k = 1, M = matH, which = 'SM', tol = 1e-3, maxiter=N*200)
+        #cond_number_M = np.linalg.cond(Mdense)
+        #cond_number_H = np.linalg.cond(Hdense)
+
+
+        #rankM = np.linalg.matrix_rank(regularized_M)
+        #rankH = np.linalg.matrix_rank(regularized_H)
+        #print("shape of matrix M = ", regularized_M.shape, flush=True)
+        #print("Rank of regularized matrix M = ", rankM, flush=True)
+        #print("shape of regularized matrix H = ", regularized_H.shape, flush=True)
+        #print("Rank of regularized matrix H = ", rankH, flush=True) 
+
+
+        PMP_sparse = csc_matrix(PMP)
+        PHP_sparse = csc_matrix(PHP)
+
+        PMP_sparse = psd_wrap(PMP_sparse)
+        PHP_sparse = psd_wrap(PHP_sparse)
+
+        #mineig2 = eigh(regularized_M, regularized_H, eigvals_only=True, subset_by_index = [0,0], driver='gvx')
+        allValues, _ = eigsh(A =  PMP_sparse, k = n-1, M = PHP_sparse, which = 'SA', tol = 1e-3, maxiter=n*200)
+        eigMaxValue, _ = eigsh(A =  PMP_sparse, k = 1, M = PHP_sparse, which = 'LA', tol = 1e-3, maxiter=n*200)
+        allValues = np.append(allValues, eigMaxValue)
+
+        rankM = (abs(allValues) > 1e-3).sum()
+
+        print("Eigenvalues = ", allValues, flush=True)
+        print("shape of eigenvalues = ", allValues.shape, flush=True)
+        print("n = ", n, flush=True)
+        print("rank of the matrix A = ", rankM, flush=True)
+
+
+        minEigenValue = allValues[n-rankM]
         maxEigenValue = allValues[-1]
         eigenValues = [minEigenValue, maxEigenValue]
 
--- representacion.py
+++ representacion.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Oct  9 21:05:16 2024
 
-@author: Ignacio Calvo Ramón-Borja
+@author: Aditya Vasudevan, modified code of Ignacio Calvo Ramón-Borja
 """
  
 # Este archivo es para ejecutar la función y obtener las gráficas
@@ -18,7 +18,7 @@ ruta_principal = '/home/aditya/Documents/locking/simulations/clamped-beam/infsup
 sys.path.append(os.path.join(ruta_principal))
 
 from lector_casos import lector_parametros, lector_unknowns, lector_matrices
-from inf_sup import mixed_infsup, primal_infsup, mixed_infsup_C
+from inf_sup import mixed_infsup, primal_infsup, mixed_infsup_C, primal_infsup_onKerB
 import matplotlib.pyplot as plt
 
 # Ruta donde se encuentran los problemas varios
@@ -63,30 +63,86 @@ for eps in epsArray:
 
 
     # Mediante un bucle aplicamos el algoritmo numérico
-    betaMin = []
-    betaMax = []
+    betaMinFromH = []
+    betaMaxFromH = []
+    betaMinFromC = []
+    betaMaxFromC = []
+    alphaMinFromA = []
+    alphaMaxFromA = []
+    alphaMaxFromAonkerB = []
+    alphaMinFromAonkerB = []
 
     count = 0
     #for M, H in zip(matsM, matsH):
-    for B, H, L, C, A in zip(matsB, matsH, matsL, matsC, matsA):
+    for A, B, C, H, L in zip(matsA, matsB, matsC, matsH, matsL):
         print("----------------------------------------------------------")
         print("Solving the eigenvalues for N = ", casos[count],flush=True)
+        print("\n\n")
         #beta.append(primal_infsup (M, H, eps))
-        eigenValues = mixed_infsup_C(B, H, C)
-        #eigenValues = primal_infsup(M, H, eps)
-        minEigenValue = eigenValues[0]
-        maxEigenValue = eigenValues[1]
-        betaMin.append(minEigenValue)
-        betaMax.append(maxEigenValue)
-        print("Maximum EigenValue = ", maxEigenValue, flush=True)
-        print("Minimum EigenValue = ", minEigenValue, flush=True)
-        count+=1
+        eigenValuesFromH = mixed_infsup(B ,H, L)
+        minEigenValue = eigenValuesFromH[0]
+        maxEigenValue = eigenValuesFromH[1]
+        betaMinFromH.append(minEigenValue)
+        betaMaxFromH.append(maxEigenValue)
+        print("Maximum EigenValue from B Hinv B.T= ", maxEigenValue, flush=True)
+        print("Minimum EigenValue from B Hinv B.T= ", minEigenValue, flush=True)
         print("----------------------------------------------------------")
-        print("\n\n")    
+        print("\n\n")
+        
+
+        eigenValuesFromC = mixed_infsup_C(B, H, C)
+        minEigenValue = eigenValuesFromC[0]
+        maxEigenValue = eigenValuesFromC[1]
+        betaMinFromC.append(minEigenValue)
+        betaMaxFromC.append(maxEigenValue)
+        print("Maximum EigenValue from B.T Cinv B = ", maxEigenValue, flush=True)
+        print("Minimum EigenValue from B.T Cinv B = ", minEigenValue, flush=True)
+        print("----------------------------------------------------------")
+        print("\n\n")
+
+        #eigenValues = primal_infsup(M, H, eps)
+        
+        eigenValuesFromA = primal_infsup(A, H)
+
+        minEigenValue = eigenValuesFromA[0]
+        maxEigenValue = eigenValuesFromA[1]
+        alphaMinFromA.append(minEigenValue)
+        alphaMaxFromA.append(maxEigenValue)
+        print("Maximum EigenValue from A = ", maxEigenValue, flush=True)
+        print("Minimum EigenValue from A = ", minEigenValue, flush=True)
+        print("----------------------------------------------------------")
+        print("\n\n")
+
+        eigenValuesFromAonkerB = primal_infsup_onKerB(A, H, B)
+
+        minEigenValue = eigenValuesFromAonkerB[0]
+        maxEigenValue = eigenValuesFromAonkerB[1]
+        alphaMinFromAonkerB.append(minEigenValue)
+        alphaMaxFromAonkerB.append(maxEigenValue)
+        print("Maximum EigenValue from A on kerB = ", maxEigenValue, flush=True)
+        print("Minimum EigenValue from A on kerB = ", minEigenValue, flush=True)
+        print("----------------------------------------------------------")
+        print("\n\n")
+
+        
+        count+=1
+
     
     # Guardo en un archivo de texto
-    np.savetxt(ruta + "/beta_h_mineig_fromC_" + str(eps) + ".txt", np.vstack((num_ecs, betaMin)))
-    np.savetxt(ruta + "/beta_h_maxeig_fromC_" + str(eps) + ".txt", np.vstack((num_ecs, betaMax)))
+
+    np.savetxt(ruta + "/beta_h_mineig_fromH.txt", np.vstack((num_ecs, betaMinFromH)))
+    np.savetxt(ruta + "/beta_h_maxeig_fromH.txt", np.vstack((num_ecs, betaMaxFromH)))
+
+    np.savetxt(ruta + "/beta_h_mineig_fromC.txt", np.vstack((num_ecs, betaMinFromC)))
+    np.savetxt(ruta + "/beta_h_maxeig_fromC.txt", np.vstack((num_ecs, betaMaxFromC)))
+
+    np.savetxt(ruta + "/alpha_h_mineig_fromA.txt", np.vstack((num_ecs, alphaMinFromA)))
+    np.savetxt(ruta + "/alpha_h_maxeig_fromA.txt", np.vstack((num_ecs, alphaMaxFromA)))
+    
+    np.savetxt(ruta + "/alpha_h_mineig_fromAOnKerB.txt", np.vstack((num_ecs, alphaMinFromAonkerB)))
+    np.savetxt(ruta + "/alpha_h_maxeig_fromAOnKerB.txt", np.vstack((num_ecs, alphaMaxFromAonkerB)))
+
+
     end_time = time.time()
     elapsed_time = end_time-start_time
     print("----------------------------------------------------------")
